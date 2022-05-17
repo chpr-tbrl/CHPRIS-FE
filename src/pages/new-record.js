df2e3d74ac71f3d5a89c19a9afd7d67f04b17673
nsedrickm <nsedrick101@gmail.com>
2022-05-17 19:06:07 +0100

    update: add schema validation for new record form

--- src/pages/new-record.js
+++ src/pages/new-record.js
@@ -15,16 +15,47 @@ import {
   RadioButton,
   RadioButtonGroup,
   Checkbox,
+  DatePicker,
+  DatePickerInput,
+  NumberInput,
 } from "@carbon/react";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { NewRecordSchema } from "schemas";
+
+const regions = [
+  {
+    id: 1,
+    text: "FCS Test Region",
+  },
+];
+const sites = [
+  {
+    id: 1,
+    text: "Afkanerd Developers",
+  },
+];
+
 const NewRecord = () => {
-  const regions = ["FCS Test Region", "West", "South-West"];
-  const sites = ["Afkanerd Developers", "They rest"];
+  const {
+    setValue,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(NewRecordSchema),
+  });
 
   const [page, setPage] = useState(0);
 
   function togglePage(index) {
     setPage(index);
+  }
+
+  async function handleRecordCreation(data) {
+    alert("form sumbitted check console for output");
+    console.log("form data: ", data);
   }
 
   return (
@@ -38,26 +69,74 @@ const NewRecord = () => {
           <Tab>Personal details</Tab>
           <Tab>Category and Symptoms</Tab>
         </TabList>
-        <Form onSubmit={(evt) => console.log(evt)}>
+        <Form onSubmit={handleSubmit(handleRecordCreation)}>
           <TabPanels>
             <TabPanel>
               <Stack gap={7}>
-                <TextInput id="name" labelText="Patient's Name" />
-                <TextInput id="age" labelText="Age" />
+                <TextInput
+                  id="records_name"
+                  labelText="Patient's Name"
+                  {...register("records_name")}
+                  invalid={errors.records_name ? true : false}
+                  invalidText={errors.records_name?.message}
+                />
+                <NumberInput
+                  id="age"
+                  label="Age"
+                  {...register("records_age")}
+                  invalid={errors.records_age ? true : false}
+                  invalidText={errors.records_age?.message}
+                  iconDescription="age"
+                />
                 <RadioButtonGroup
                   legendText="Sex"
-                  name="Sex"
+                  name="records_sex"
                   defaultSelected="female"
+                  onChange={(evt) => setValue("records_sex", evt)}
                 >
                   <RadioButton labelText="Female" value="female" id="female" />
                   <RadioButton labelText="Male" value="male" id="male" />
                 </RadioButtonGroup>
-                <TextInput id="phone_number" labelText="Phone Number" />
-                <TextInput id="phone_number2" labelText="Phone Number (2)" />
+
+                <DatePicker datePickerType="single">
+                  <DatePickerInput
+                    placeholder="mm/dd/yyyy"
+                    labelText="Date of test request"
+                    id="records_date_of_test_request"
+                    {...register("records_date_of_test_request")}
+                    defaultValue={new Date()}
+                    invalid={errors.records_date_of_test_request ? true : false}
+                    invalidText={errors.records_date_of_test_request?.message}
+                  />
+                </DatePicker>
+
+                <TextInput
+                  id="records_address"
+                  labelText="Address"
+                  {...register("records_address")}
+                  invalid={errors.records_address ? true : false}
+                  invalidText={errors.records_address?.message}
+                />
+
+                <TextInput
+                  id="records_telephone"
+                  labelText="Telephone"
+                  {...register("records_telephone")}
+                  invalid={errors.records_telephone ? true : false}
+                  invalidText={errors.records_telephone?.message}
+                />
+                <TextInput
+                  id="records_telephone_2"
+                  labelText="Telephone(2)"
+                  {...register("records_telephone_2")}
+                />
                 <RadioButtonGroup
                   legendText="Has ART unique code"
-                  name="Has ART unique code"
+                  name="records_has_art_unique_code"
                   defaultSelected="unknown"
+                  onChange={(evt) =>
+                    setValue("records_has_art_unique_code", evt)
+                  }
                 >
                   <RadioButton labelText="yes" value="yes" id="yes" />
                   <RadioButton labelText="no" value="no" id="no" />
@@ -68,15 +147,22 @@ const NewRecord = () => {
                   />
                 </RadioButtonGroup>
 
+                <TextInput
+                  id="records_art_unique_code"
+                  labelText="Record ART Unique code"
+                  {...register("records_art_unique_code")}
+                />
+
                 <RadioButtonGroup
                   legendText="Status"
                   name="status"
-                  defaultSelected="out-patient"
+                  defaultSelected="outpatient"
+                  onChange={(evt) => setValue("records_status", evt)}
                 >
                   <RadioButton
                     labelText="Outpatient"
-                    value="out-patient"
-                    id="out-patient"
+                    value="outpatient"
+                    id="outpatient"
                   />
                   <RadioButton
                     labelText="Ward-Bed"
@@ -85,10 +171,19 @@ const NewRecord = () => {
                   />
                 </RadioButtonGroup>
 
+                <TextInput
+                  id="records_ward_bed_number"
+                  labelText="Ward bed number"
+                  {...register("records_ward_bed_number")}
+                />
+
                 <RadioButtonGroup
                   legendText="Currently pregnant"
                   name="currently pregnant"
                   defaultSelected="no"
+                  onChange={(evt) =>
+                    setValue("records_currently_pregnant", evt)
+                  }
                 >
                   <RadioButton labelText="yes" value="yes" id="yes" />
                   <RadioButton labelText="no" value="no" id="no" />
@@ -101,27 +196,84 @@ const NewRecord = () => {
             <TabPanel>
               <Stack gap={7}>
                 <FormGroup legendText="Symptoms">
-                  <Checkbox labelText="Current cough" id="current-cough" />
-                  <Checkbox labelText="Fever" id="fever" />
-                  <Checkbox labelText="Night sweats" id="night-sweats" />
-                  <Checkbox labelText="Weight loss" id="weight-loss" />
-                  <Checkbox labelText="None of the above" id="none" />
+                  <Checkbox
+                    labelText="Current cough"
+                    id="records_symptoms_current_cough"
+                    {...register("records_symptoms_current_cough")}
+                  />
+                  <Checkbox
+                    labelText="Fever"
+                    id="records_symptoms_fever"
+                    {...register("records_symptoms_fever")}
+                  />
+                  <Checkbox
+                    labelText="Night sweats"
+                    id="records_symptoms_night_sweats"
+                    {...register("records_symptoms_night_sweats")}
+                  />
+                  <Checkbox
+                    labelText="Weight loss"
+                    id="records_symptoms_weight_loss"
+                    {...register("records_symptoms_weight_loss")}
+                  />
+                  <Checkbox
+                    labelText="None of the above"
+                    id="records_symptoms_none_of_the_above"
+                    {...register("records_symptoms_none_of_the_above")}
+                  />
                 </FormGroup>
 
                 <FormGroup legendText="Patient Category">
-                  <Checkbox labelText="Hospitalized (HOS)" id="hospitalized" />
-                  <Checkbox labelText="Child (CHI)" id="child" />
-                  <Checkbox labelText="To initiate ART(ART)" id="art" />
-                  <Checkbox labelText="On ART symptomatic (PLH)" id="" />
-                  <Checkbox labelText="Out Patient" id="" />
-                  <Checkbox labelText="ANC" id="" />
-                  <Checkbox labelText="Diabetes clinic" id="" />
-                  <Checkbox labelText="Other" id="" />
-                  <TextInput id="other-value" labelText="Other" />
+                  <Checkbox
+                    labelText="Hospitalized (HOS)"
+                    id="records_patient_category_hospitalized"
+                    {...register("records_patient_category_hospitalized")}
+                  />
+                  <Checkbox
+                    labelText="Child (CHI)"
+                    id="records_patient_category_child"
+                    {...register("records_patient_category_child")}
+                  />
+                  <Checkbox
+                    labelText="To initiate ART(ART)"
+                    id="records_patient_category_to_initiate_art"
+                    {...register("records_patient_category_to_initiate_art")}
+                  />
+                  <Checkbox
+                    labelText="On ART symptomatic (PLH)"
+                    id="records_patient_category_on_art_symptoma
+                    tic"
+                    {...register("records_patient_category_on_art_symptomatic")}
+                  />
+                  <Checkbox
+                    labelText="Out Patient"
+                    id="records_patient_category_outpatient"
+                    {...register("records_patient_category_outpatient")}
+                  />
+                  <Checkbox
+                    labelText="ANC"
+                    id="records_patient_category_anc"
+                    {...register("records_patient_category_anc")}
+                  />
+                  <Checkbox
+                    labelText="Diabetes clinic"
+                    id="records_patient_category_diabetes_clinic"
+                    {...register("records_patient_category_diabetes_clinic")}
+                  />
+                  <Checkbox labelText="Other" id="has_orther" />
+                  <TextInput
+                    id="records_patient_category_other"
+                    labelText="Other"
+                    {...register("records_patient_category_other")}
+                  />
                 </FormGroup>
 
                 <FormGroup legendText="Reason for test">
-                  <Checkbox labelText="Presumptive TB (Pre-treatment)" id="" />
+                  <Checkbox
+                    labelText="Presumptive TB (Pre-treatment)"
+                    id="records_reason_for_test_presumptive_tb"
+                    {...register("records_reason_for_test_presumptive_tb")}
+                  />
                 </FormGroup>
 
                 <FormGroup legendText="TB Treatment history">
@@ -131,6 +283,9 @@ const NewRecord = () => {
                       name="tth"
                       defaultSelected="new"
                       orientation="vertical"
+                      onChange={(evt) =>
+                        setValue("records_tb_treatment_history", evt)
+                      }
                     >
                       <RadioButton
                         labelText="New (never treated > 1 month"
@@ -139,13 +294,13 @@ const NewRecord = () => {
                       />
                       <RadioButton
                         labelText="Relapse (completed previous TB Rx)"
-                        value="re"
-                        id="re"
+                        value="relapse"
+                        id="relapse"
                       />
                       <RadioButton
                         labelText="After loss to follow up(>= 2 months of Tx interruption)"
-                        value="al"
-                        id="al"
+                        value="after_loss_to_follow_up"
+                        id="after_loss_to_follow_up"
                       />
                       <RadioButton
                         labelText="Failure (2 smears AFB positive at >= 5 months on Rx)"
@@ -154,7 +309,13 @@ const NewRecord = () => {
                       />
                     </RadioButtonGroup>
                     <Checkbox labelText="contact of TB patient" id="contact" />
-                    <TextInput id="tbt-other-value" labelText="Other" />
+                    <TextInput
+                      labelText="Other"
+                      id="records_tb_treatment_history_contact_of_tb_patient"
+                      {...register(
+                        "records_tb_treatment_history_contact_of_tb_patient"
+                      )}
+                    />
                   </Stack>
                 </FormGroup>
 
@@ -165,14 +326,28 @@ const NewRecord = () => {
                       titleText="Region"
                       label="Select region"
                       items={regions}
-                      itemToString={(item) => item}
+                      itemToString={(item) => item.text}
+                      invalid={errors.region_id ? true : false}
+                      invalidText={errors.region_id?.message}
+                      onChange={(evt) =>
+                        setValue("region_id", evt.selectedItem.id, {
+                          shouldValidate: true,
+                        })
+                      }
                     />
                     <Dropdown
                       id="site"
                       titleText="Site"
                       label="Select site"
                       items={sites}
-                      itemToString={(item) => item}
+                      itemToString={(item) => item.text}
+                      invalid={errors.site_id ? true : false}
+                      invalidText={errors.site_id?.message}
+                      onChange={(evt) =>
+                        setValue("site_id", evt.selectedItem.id, {
+                          shouldValidate: true,
+                        })
+                      }
                     />
                   </Stack>
                 </FormGroup>
---
+++ src/schemas/new-record.js
@@ -0,0 +1,61 @@
+import * as yup from "yup";
+
+export const NewRecordSchema = yup.object({
+  site_id: yup.number().positive().integer().required("Field is required"),
+  region_id: yup.number().positive().integer().required("Field is required"),
+  records_age: yup
+    .number()
+    .positive("Field is required")
+    .integer()
+    .required("Field is required"),
+  records_name: yup.string().required("Field is required"),
+  records_sex: yup
+    .string()
+    .oneOf(["male", "female"])
+    .default("female")
+    .required("Field is required"),
+  records_date_of_test_request: yup
+    .date()
+    .default(() => new Date())
+    .required("Field is required"),
+  records_address: yup.string().required("Field is required"),
+  records_telephone: yup.string().required("Field is required"),
+  records_telephone_2: yup.string(),
+  records_has_art_unique_code: yup
+    .string()
+    .oneOf(["yes", "no", "unknown"])
+    .default("unknown")
+    .required("Field is required"),
+  records_art_unique_code: yup.string(),
+  record_status: yup
+    .string()
+    .oneOf(["outpatient", "ward-bed"])
+    .default("outpatient")
+    .required("Field is required"),
+  records_ward_bed_number: yup.string(),
+  records_currently_pregnant: yup
+    .string()
+    .oneOf(["yes", "no"])
+    .default("no")
+    .required("Field is required"),
+  records_symptoms_current_cough: yup.bool(),
+  records_symptoms_fever: yup.bool(),
+  records_symptoms_night_sweats: yup.bool(),
+  records_symptoms_weight_loss: yup.bool(),
+  records_symptoms_none_of_the_above: yup.bool(),
+  records_patient_category_hospitalized: yup.bool(),
+  records_patient_category_child: yup.bool(),
+  records_patient_category_to_initiate_art: yup.bool(),
+  records_patient_category_on_art_symptomatic: yup.bool(),
+  records_patient_category_outpatient: yup.bool(),
+  records_patient_category_anc: yup.bool(),
+  records_patient_category_diabetes_clinic: yup.bool(),
+  records_patient_category_other: yup.string(),
+  records_reason_for_test_presumptive_tb: yup.bool(),
+  records_tb_treatment_history: yup
+    .string()
+    .oneOf(["new", "relapse", "after_loss_to_follow_up", "failure"])
+    .default("new")
+    .required("Field is required"),
+  records_tb_treatment_history_contact_of_tb_patient: yup.string(),
+});
