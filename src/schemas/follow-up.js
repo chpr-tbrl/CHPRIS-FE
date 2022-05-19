3f08dee8267af450ba9607e7e5af1c2a105481ac
nsedrickm <nsedrick101@gmail.com>
2022-05-19 06:50:43 +0100

    update: add follow-up schema

---
+++ src/schemas/follow-up.js
@@ -0,0 +1,12 @@
+import * as yup from "yup";
+
+export const FOLLOW_UP_SCHEMA = yup.object({
+  follow_up_xray: yup.bool(),
+  follow_up_amoxicillin: yup.bool(),
+  follow_up_other_antibiotic: yup.string(),
+  follow_up_schedule_date: yup
+    .date()
+    .default(() => new Date())
+    .required("Field is required"),
+  follow_up_comments: yup.string(),
+});
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,3 +2,4 @@ export * from "./new-record";
 export * from "./signup";
 export * from "./login";
 export * from "./lab-results";
+export * from "./follow-up";
