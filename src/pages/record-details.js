e49bf4feb861d20957d1a5bd07d1878cfefba1f8
nsedrickm <nsedrick101@gmail.com>
2022-05-20 14:11:11 +0100

    update: display record information

--- src/pages/record-details.js
+++ src/pages/record-details.js
@@ -1,72 +1,149 @@
 import React from "react";
-import { PageHeader, TabBar } from "components";
+import { PageHeader, Spacer, TabBar } from "components";
 import { Button, FormLabel, FlexGrid, Row, Column } from "@carbon/react";
 import { useSelector } from "react-redux";
 import { recordSelector } from "features";
-
+import { Person } from "@carbon/icons-react";
 const RecordDetails = () => {
   const record = useSelector(recordSelector);
-
   return (
-    <div className="page">
+    <FlexGrid fullWidth className="page">
       <TabBar />
       <PageHeader
         title="Information"
         description="Manage and update record information"
+        renderIcon={<Person size={42} />}
       />
 
-      <FlexGrid fullWidth condensed>
-        <Row>
-          <Column>
-            <FormLabel>ID</FormLabel>
-            <p>{record?.id}</p>
-            <br />
-          </Column>
-          <Column>
-            <FormLabel>Patient's name</FormLabel>
-            <p>{record?.name}</p>
-            <br />
-          </Column>
-        </Row>
-        <Row>
-          <Column>
-            <FormLabel>Telephone</FormLabel>
-            <p>658794635</p>
-            <br />
-          </Column>
-          <Column>
-            <FormLabel>Alternative Telephone</FormLabel>
-            <p>658794635</p>
-            <br />
-          </Column>
-        </Row>
-        <Row>
-          <Column>
-            <FormLabel>Sex</FormLabel>
-            <p>Female</p>
-            <br />
-            <FormLabel>Date of test request</FormLabel>
-            <p>{record?.created}</p>
-            <br />
-            <FormLabel>Address</FormLabel>
-            <p>Here</p>
-            <br />
-          </Column>
-          <Column>
-            <FormLabel>ART</FormLabel>
-            <p>lorem ipsum</p>
-            <br />
-            <FormLabel>Status</FormLabel>
-            <p>Out patient</p>
-            <br />
-          </Column>
-        </Row>
-        <Row>
-          <br />
-          <Button>Edit</Button>
-        </Row>
-      </FlexGrid>
-    </div>
+      <Row>
+        <Column>
+          <FormLabel>Id</FormLabel>
+          <p>{record.record_id + "" || "N/A"}</p>
+          <Spacer h={5} />
+          <FormLabel>Patient's name</FormLabel>
+          <p>{record.records_name + "" || "N/A"}</p>
+          <Spacer h={5} />
+          <FormLabel>Age</FormLabel>
+          <p>{record.records_age + "" || "N/A"}</p>
+          <Spacer h={5} />
+          <FormLabel>Sex</FormLabel>
+          <p>{record.records_sex + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>date_of_test_request</FormLabel>
+          <p>{record.records_date_of_test_request + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>address</FormLabel>
+          <p>{record.records_address + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>telephone</FormLabel>
+          <p>{record.records_telephone + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>telephone_2</FormLabel>
+          <Spacer h={5} />
+
+          <FormLabel>has_art_unique_code</FormLabel>
+          <p>{record.records_has_art_unique_code + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>art_unique_code</FormLabel>
+          <p>{record.records_art_unique_code + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>status</FormLabel>
+          <p>{record.records_status + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>ward_bed_number</FormLabel>
+          <p>{record.records_ward_bed_number + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>currently_pregnant</FormLabel>
+          <p>{record.records_currently_pregnant + "" || "N/A"}</p>
+          <Spacer h={5} />
+        </Column>
+        <Column>
+          <FormLabel>symptoms_current_cough</FormLabel>
+          <p>{record.records_symptoms_current_cough + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>symptoms_fever</FormLabel>
+          <p>{record.records_symptoms_fever + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>symptoms_night_sweats</FormLabel>
+          <p>{record.records_symptoms_night_sweats + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>symptoms_weight_loss</FormLabel>
+          <p>{record.records_symptoms_weight_loss + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>symptoms_none_of_the_above</FormLabel>
+          <p>{record.records_symptoms_none_of_the_above + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>patient_category_hospitalized</FormLabel>
+          <p>{record.records_patient_category_hospitalized + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>patient_category_child</FormLabel>
+          <p>{record.records_patient_category_child + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>patient_category_to_initiate_art</FormLabel>
+          <p>{record.records_patient_category_to_initiate_art + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>patient_category_on_art_symptomatic</FormLabel>
+          <p>
+            {record.records_patient_category_on_art_symptomatic + "" || "N/A"}
+          </p>
+          <Spacer h={5} />
+
+          <FormLabel>patient_category_outpatient</FormLabel>
+          <p>{record.records_patient_category_outpatient + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>patient_category_anc</FormLabel>
+          <p>{record.records_patient_category_anc + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <p>{record.records_patient_category_diabetes_clinic + "" || "N/A"}</p>
+          <FormLabel>patient_category_diabetes_clinic</FormLabel>
+          <Spacer h={5} />
+
+          <FormLabel>patient_category_other</FormLabel>
+          <p>{record.records_patient_category_other + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>reason_for_test_presumptive_tb</FormLabel>
+          <p>{record.records_reason_for_test_presumptive_tb + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>tb_treatment_history</FormLabel>
+          <p>{record.records_tb_treatment_history + "" || "N/A"}</p>
+          <Spacer h={5} />
+
+          <FormLabel>
+            records_tb_treatment_history_contact_of_tb_patient
+          </FormLabel>
+          <p>
+            {record.records_tb_treatment_history_contact_of_tb_patient + "" ||
+              "N/A"}
+          </p>
+          <Spacer h={5} />
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <Button>Edit record</Button>
+        </Column>
+      </Row>
+    </FlexGrid>
   );
 };
 
