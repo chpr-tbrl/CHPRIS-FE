d4e0e71e9c4e5b30de63780c56a3a530df2d0181
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-07-13 14:17:06 +0100

    fix(specimen collection): collection 2 required error

--- src/schemas/specimen-collection.js
+++ src/schemas/specimen-collection.js
@@ -49,27 +49,33 @@ export const SPECIMEN_COLLECTION_SCHEMA = yup.object({
   specimen_collection_1_received_by: yup.string().required("Field is required"),
   specimen_collection_2_date: yup
     .date()
-    .typeError("Field is required")
-    .required("Field is required"),
-
+    .nullable()
+    .transform((v) => (v instanceof Date && !isNaN(v) ? v : null))
+    .typeError("Field is required"),
   specimen_collection_2_specimen_collection_type: yup
     .string()
-    .oneOf([
-      "sputum",
-      "csf",
-      "lymph_node_aspirate",
-      "gastric_aspirate",
-      "urine",
-      "abscess",
-      "bronchoalveolar_aspirate",
-      "isolate_from_sputum",
-      "isolate_from_specimen_not_sputum",
-      "pleural_fluid",
-      "unk_unknown",
-      "other"
-    ])
-    .default("sputum")
-    .required("Field is required"),
+    .nullable()
+    .default("")
+    .when("specimen_collection_2_date", {
+      is: (value) => value != null,
+      then: (schema) =>
+        schema
+          .oneOf([
+            "sputum",
+            "csf",
+            "lymph_node_aspirate",
+            "gastric_aspirate",
+            "urine",
+            "abscess",
+            "bronchoalveolar_aspirate",
+            "isolate_from_sputum",
+            "isolate_from_specimen_not_sputum",
+            "pleural_fluid",
+            "unk_unknown",
+            "other",
+          ])
+          .required("Field is required"),
+    }),
   specimen_collection_2_other: yup
     .string()
     .default("")
@@ -79,19 +85,28 @@ export const SPECIMEN_COLLECTION_SCHEMA = yup.object({
     }),
   specimen_collection_2_period: yup
     .string()
-    .oneOf(["spot", "morning", "n_a"])
-    .default("spot")
+    .nullable()
+    .default("")
     .when("specimen_collection_2_specimen_collection_type", {
       is: "sputum",
-      then: (schema) => schema.required("Field is required"),
+      then: (schema) =>
+        schema.oneOf(["spot", "morning", "n_a"]).required("Field is required"),
     }),
   specimen_collection_2_aspect: yup
     .string()
-    .oneOf(["mucopurulent", "bloody", "salivary", "n_a"])
-    .default("mucopurulent")
+    .nullable()
+    .default("")
     .when("specimen_collection_2_specimen_collection_type", {
       is: "sputum",
+      then: (schema) =>
+        schema
+          .oneOf(["mucopurulent", "bloody", "salivary", "n_a"])
+          .required("Field is required"),
+    }),
+  specimen_collection_2_received_by: yup
+    .string()
+    .when("specimen_collection_2_date", {
+      is: (value) => value != null,
       then: (schema) => schema.required("Field is required"),
     }),
-  specimen_collection_2_received_by: yup.string().required("Field is required"),
 });
